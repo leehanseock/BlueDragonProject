3c763d15a6970db91270f4a172153178977bf32b
B612-jujin <jujingood@gmail.com>
2024-11-22 15:13:46 +0900

    Notice.jsx 서버통신 수정

--- src/Notice/Notice.jsx
+++ src/Notice/Notice.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import '../can/Gallery/Gallery.css';
 import Header from "../MainResource/Header";
+import {Link} from "react-router-dom";
+import {fetchPosts} from "../api";
 
 
 function PostRow({ number, title, author, date, views }) {
     return (
-        <tr>
+        <tr className="ABB-notice-row">
             <td>{number}</td>
-            <td><a href="#">{title}</a></td>
+            <td><Link to={`/posts/${number}`}>{title}</Link></td>
             <td>{author}</td>
             <td>{date}</td>
             <td>{views}</td>
@@ -15,22 +17,24 @@ function PostRow({ number, title, author, date, views }) {
     );
 }
 
-function PostList({ currentPage, itemsPerPage }) {
-    const posts = [
-        { number: '공지', title: '2024년 최신 IT 트렌드 공유', author: '운영자', date: '2024-11-05', views: 462 },
-        { number: '813', title: '효율적인 업무 관리 도구 추천 리스트', author: '효율회원', date: '2024-11-05', views: 343 },
-        { number: '812', title: '개발자들이 자주 사용하는 무료 툴 모음!', author: '개발자회원', date: '2024-11-05', views: 24 },
-        { number: '811', title: '온라인 마케팅 성공 사례 공유하기', author: '마케팅회원', date: '2024-11-05', views: 30 },
-    ];
-
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentPosts = posts.slice(startIndex, endIndex);
+const PostList = ({ currentPage, itemsPerPage }) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const loadPosts = async () => {
+            try {
+                const data = await fetchPosts();
+                setPosts(data.posts);
+            } catch (err) {
+                console.error('Error fetching posts:', err);
+            }
+        };
+        loadPosts();
+    }, [currentPage, itemsPerPage]);
 
     return (
-        <table>
+        <table className="ABB-post-table">
             <thead>
-            <tr>
+            <tr className="ABB-post-header">
                 <th>번호</th>
                 <th>제목</th>
                 <th>작성자</th>
@@ -39,61 +43,71 @@ function PostList({ currentPage, itemsPerPage }) {
             </tr>
             </thead>
             <tbody>
-            {currentPosts.map((post, index) => (
-                <PostRow key={index} {...post} />
-            ))}
+            {posts.length === 0 ? (
+                <tr className="ABB-no-posts">
+                    <td colSpan="5">게시물이 없습니다.</td>
+                </tr>
+            ) : (
+                posts.map((post) => (
+                    <PostRow
+                        key={post.post_id}
+                        number={post.post_id}
+                        title={post.title}
+                        author={post.author}
+                        date={post.created_at}
+                        views={post.views}
+                    />
+                ))
+            )}
             </tbody>
         </table>
     );
-}
+};
 
-const Pagination = ({ totalPosts, itemsPerPage, currentPage, setCurrentPage }) => {
-    const totalPages = Math.ceil(totalPosts / itemsPerPage);
+const Pagination = ({ totalPages, currentPage, setCurrentPage }) => {
     const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
-
-    const handlePageChange = (pageNumber) => {
-        if (pageNumber >= 1 && pageNumber <= totalPages) {
-            setCurrentPage(pageNumber);
-        }
-    };
-
-    const handlePrevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
-
-    const handleNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
 
     return (
         <div className="pagination">
-            <a href="#" onClick={handlePrevPage}>&laquo; 이전</a>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a href="#"
+               onClick={(e) => {
+                   e.preventDefault();
+                   if (currentPage > 1) setCurrentPage(currentPage - 1);
+               }}
+            >
+                &laquo; 이전
+            </a>
             {pageNumbers.map((number) => (
                 <a
                     href="#"
                     key={number}
                     className={number === currentPage ? 'active' : ''}
-                    onClick={() => handlePageChange(number)}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setCurrentPage(number);
+                    }}
                 >
                     {number}
                 </a>
             ))}
-            <a href="#" onClick={handleNextPage}>다음 &raquo;</a>
+            <a href="#"
+               onClick={(e) => {
+                   e.preventDefault();
+                   if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+               }}
+            >
+                다음 &raquo;
+            </a>
         </div>
     );
 };
 
-
-
 function Notice() {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 5; // Number of items per page
-    const totalPosts = 10; // Total number of posts
+    const [totalPages, setTotalPages] = useState(1);
+    const itemsPerPage = 10;
 
     return (
         <div>
@@ -102,8 +116,7 @@ function Notice() {
                 <h2>공지사항</h2>
                 <PostList currentPage={currentPage} itemsPerPage={itemsPerPage} />
                 <Pagination
-                    totalPosts={totalPosts}
-                    itemsPerPage={itemsPerPage}
+                    totalPages={totalPages}
                     currentPage={currentPage}
                     setCurrentPage={setCurrentPage}
                 />
